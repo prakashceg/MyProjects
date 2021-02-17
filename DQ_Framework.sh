2eb8c3d4bbcb41f4cd5042e01caa389cd5b78976
prakashceg <prakash.mcn@gmail.com>
2021-02-17 10:56:49 +0530

    Add files via upload

---
+++ DQ_Framework.sh
@@ -0,0 +1,10 @@
+cd /bi_az/bi_ci/CheckFrmwork
+
+export LD_LIBRARY_PATH=/u01/oracle/product/client1120/lib
+export ORACLE_HOME=/u01/oracle/product/client1120
+
+wfname=$1
+
+echo "$wfname"
+
+python2.7 /bi_az/bi_ci/CheckFrmwork/CheckPy.py "$wfname">/bi_az/bi_ci/CheckFrmwork/log1.txt
