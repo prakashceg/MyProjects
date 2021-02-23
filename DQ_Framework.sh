b66cadc29daacb72beaf965a692de311312b387a
prakashceg <prakash.mcn@gmail.com>
2021-02-23 18:47:41 +0530

    Update DQ_Framework.sh

--- DQ_Framework.sh
+++ DQ_Framework.sh
@@ -1,4 +1,4 @@
-cd /bi_az/bi_ci/CheckFrmwork
+cd /db_ap/db_ai/CheckFrmwork
 
 export LD_LIBRARY_PATH=/u01/oracle/product/client1120/lib
 export ORACLE_HOME=/u01/oracle/product/client1120
@@ -7,4 +7,4 @@ wfname=$1
 
 echo "$wfname"
 
-python2.7 /bi_az/bi_ci/CheckFrmwork/CheckPy.py "$wfname">/bi_az/bi_ci/CheckFrmwork/log1.txt
+python2.7 /db_ap/db_ai/CheckFrmwork/CheckPy.py "$wfname">/bi_az/bi_ci/CheckFrmwork/log1.txt
